4925cd6d1487d8ecef6095d40c46de99df1dc222
nupurpandey <y>
2019-07-25 17:39:05 +0530

    Product update

--- product/src/main/java/com/coviam/product/controller/ProductController.java
+++ product/src/main/java/com/coviam/product/controller/ProductController.java
@@ -4,20 +4,29 @@ import com.coviam.product.entity.Category;
 import com.coviam.product.entity.Product;
 import com.coviam.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/getAllProducts/{categoryName}")
     public List<Product> getAllProductByCategory(@PathVariable String categoryName) {
         return productService.getProductByCategory(categoryName);
     }
+
 
     @GetMapping("/getAProduct/{productId}")
     public List<Product> getProductByProductId(@PathVariable String productId) {
@@ -48,5 +57,10 @@ public class ProductController {
         return productService.getAllCategories();
     }
 
+    private static HttpEntity<?> getHeaders() throws IOException{
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<String>(headers);
+    }
 
 }
