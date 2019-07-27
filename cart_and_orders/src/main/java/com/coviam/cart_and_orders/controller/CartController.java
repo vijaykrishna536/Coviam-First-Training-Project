946058e4e5d76b060b2a16da8c6ac05e2add5dfc
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-07-27 17:46:58 +0530

    update

--- cart_and_orders/src/main/java/com/coviam/cart_and_orders/controller/CartController.java
+++ cart_and_orders/src/main/java/com/coviam/cart_and_orders/controller/CartController.java
@@ -3,18 +3,17 @@ package com.coviam.cart_and_orders.controller;
 import com.coviam.cart_and_orders.dto.CartDto;
 import com.coviam.cart_and_orders.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
+
 public class CartController {
 
     @Autowired
     CartService cartService;
 
-    @PostMapping("/addCart/{customerId}")
+    @GetMapping("/addCart/{customerId}")
     public String addCart(@PathVariable("customerId") Long customerId){
         CartDto cartDto=cartService.addCart(customerId);
         return cartDto.getId();
